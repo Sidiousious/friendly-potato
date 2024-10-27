94b4d6f6cf0bb0335e878c49e75cdec0f7e29b80
Sidiousious <9719080-Sidiousious@users.noreply.gitlab.com>
2024-10-27 03:50:19 +0300

    feat: add directional icon to player list; fix: reflect current target as selected in the list

--- FriendlyPotato/Windows/DtrClickWindow.cs
+++ FriendlyPotato/Windows/DtrClickWindow.cs
@@ -2,6 +2,8 @@
 using System.Linq;
 using System.Numerics;
 using Dalamud.Game.ClientState.Keys;
+using Dalamud.Game.ClientState.Objects.SubKinds;
+using Dalamud.Interface;
 using Dalamud.Interface.Windowing;
 using ImGuiNET;
 
@@ -11,8 +13,7 @@ public class DtrClickWindow : Window, IDisposable
 {
     private readonly PlayerInformation playerInformation;
     private readonly Configuration configuration;
-    private int selectedIdx = -1;
-            
+
     private readonly string[] healers = ["AST", "WHM", "SCH", "SGE"];
 
     // We give this window a constant ID using ###
@@ -48,10 +49,10 @@ public class DtrClickWindow : Window, IDisposable
             return;
         }
 
+        var cameraAimAngle = AimAngle(AimVector2());
+
         var drawnPlayers = playerInformation.Players
-#if !DEBUG
                                             .Where(p => p.IsKind(PlayerCharacterKind.Dead) || p.IsKind(PlayerCharacterKind.Doomed))
-#endif
                                             .ToList();
         drawnPlayers.Sort((a, b) =>
         {
@@ -87,9 +88,19 @@ public class DtrClickWindow : Window, IDisposable
                 return 1;
             }
 
+            if (a.Character.IsTargetable && !b.Character.IsTargetable)
+            {
+                return -1;
+            }
+
+            if (!a.Character.IsTargetable && b.Character.IsTargetable)
+            {
+                return 1;
+            }
+
             return string.Compare(a.Character.Name.ToString(), b.Character.Name.ToString(), StringComparison.Ordinal);
         });
-        
+
         ImGui.Text(string.Join(" & ", typeHeaders) + " Players:");
         for (var i = 0; i < drawnPlayers.Count; i++)
         {
@@ -101,7 +112,7 @@ public class DtrClickWindow : Window, IDisposable
                 raiseText = " (dead)";
                 color = new Vector4(1f, 0.6f, 0.6f, 1f);
             }
-            
+
             if (player.Raised)
             {
                 raiseText = " (raised)";
@@ -113,12 +124,27 @@ public class DtrClickWindow : Window, IDisposable
                 raiseText = " (doomed)";
                 color = new Vector4(1f, 0.6f, 1f, 1f);
             }
-            
+
+            var selectableFlags = ImGuiSelectableFlags.None;
+            if (!player.Character.IsTargetable)
+            {
+                color.W *= 0.8f;
+                raiseText += " (untargetable)";
+                selectableFlags = ImGuiSelectableFlags.Disabled;
+            }
+
+            var angleToTarget = AngleToTarget(player.Character, cameraAimAngle);
+            var icon = DirectionArrow(angleToTarget);
+
             var text = $"[{player.JobAbbreviation}] {player.Character.Name}  {player.Character.HomeWorld.GameData?.Name ?? "Unknown"}{raiseText}";
             ImGui.PushStyleColor(ImGuiCol.Text, color);
-            if (ImGui.Selectable(text, selectedIdx == i))
+            using (FriendlyPotato.PluginInterface.UiBuilder.IconFontFixedWidthHandle.Push())
             {
-                selectedIdx = i;
+                ImGui.Text(icon.ToIconString());
+                ImGui.SameLine();
+            }
+            if (ImGui.Selectable(text, FriendlyPotato.TargetManager.Target?.GameObjectId == player.Character.GameObjectId, selectableFlags))
+            {
                 if (FriendlyPotato.KeyState[VirtualKey.CONTROL])
                 {
                     FriendlyPotato.TargetManager.FocusTarget = player.Character;
@@ -131,5 +157,45 @@ public class DtrClickWindow : Window, IDisposable
 
             ImGui.PopStyleColor(1);
         }
+    }
+
+    private static FontAwesomeIcon DirectionArrow(double angle)
+    {
+        return angle switch
+        {
+            > -45 and < 45 => FontAwesomeIcon.ArrowCircleUp,     // up arrow
+            <= -135 or >= 135 => FontAwesomeIcon.ArrowCircleDown,  // back arrow
+            <= -45 and > -135 => FontAwesomeIcon.ArrowCircleLeft,  // left arrow
+            >= 45 and < 135 => FontAwesomeIcon.ArrowCircleRight,    // right arrow
+            _ => FontAwesomeIcon.Question
+        };
+    }
+
+    private static double AngleToTarget(IPlayerCharacter character, double aimAngle)
+    {
+        var dirVector3 = FriendlyPotato.ClientState.LocalPlayer!.Position - character.Position;
+        var dirVector = Vector2.Normalize(new Vector2(dirVector3.X, dirVector3.Z));
+        var dirAngle = AimAngle(dirVector);
+        var angularDifference = dirAngle - aimAngle;
+        switch (angularDifference)
+        {
+            case > 180:
+                angularDifference -= 360;
+                break;
+            case < -180:
+                angularDifference += 360;
+                break;
+        }
+
+        return angularDifference;
+    }
+
+    private static double AimAngle(Vector2 aimVector) => Math.Atan2(aimVector.Y, aimVector.X) * 180f / Math.PI;
+
+    private static unsafe Vector2 AimVector2()
+    {
+        var camera = FFXIVClientStructs.FFXIV.Client.Graphics.Scene.CameraManager.Instance()->CurrentCamera;
+        var threeDAim = new Vector3(camera->RenderCamera->Origin.X, camera->RenderCamera->Origin.Y, camera->RenderCamera->Origin.Z) - FriendlyPotato.ClientState.LocalPlayer!.Position;
+        return Vector2.Normalize(new Vector2(threeDAim.X, threeDAim.Z));
     }
 }
